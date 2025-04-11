86518e1260be10822aff24e2397cdefbb5b670c2
Yuji Orita <yorita@labellevie.jp>
2025-04-11 13:19:20 +0900

    buttonをcomponentsに記載してみる

--- src/lib/components/button/Button.svelte
+++ src/lib/components/button/Button.svelte
@@ -1,6 +1,32 @@
 <script lang="ts">
-  import { button } from './button.css';
+  import { css } from 'styled-system/css';
+
   export let label: string = 'ログイン';
+
+  const buttonStyle = css({
+    display: 'block',
+    width: '100%', 
+    padding: '12px 24px',
+    backgroundColor: '#C9302C',
+    color: 'white',
+    border: 'none',
+    borderRadius: '0px',
+    cursor: 'pointer',
+    fontSize: '16px',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    textDecoration: 'none',
+  
+    boxSizing: 'border-box',
+  
+    '&:hover': {
+      opacity: 0.9,
+    },
+  
+    '&:active': {
+      opacity: 0.8,
+    },
+  })
 </script>
 
-<button class={button}>{label}</button>
+<button class={buttonStyle}>{label}</button>
