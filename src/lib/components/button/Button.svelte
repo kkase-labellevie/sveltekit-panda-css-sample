fc89d40ccafc6ca0f10e635200cd4c5851ee9f23
Yuji Orita <yorita@labellevie.jp>
2025-04-11 13:31:34 +0900

    buttonをcomponentsに記載してみる

--- src/lib/components/button/Button.svelte
+++ src/lib/components/button/Button.svelte
@@ -30,3 +30,30 @@
 </script>
 
 <button class={buttonStyle}>{label}</button>
+
+<!-- このようにも書ける -->
+<!-- <button class={css({
+  display: 'block',
+  width: '100%', 
+  padding: '12px 24px',
+  backgroundColor: '#C9302C',
+  color: 'white',
+  border: 'none',
+  borderRadius: '0px',
+  cursor: 'pointer',
+  fontSize: '16px',
+  fontWeight: 'bold',
+  textAlign: 'center',
+  textDecoration: 'none',
+
+  boxSizing: 'border-box',
+
+  '&:hover': {
+    opacity: 0.9,
+  },
+
+  '&:active': {
+    opacity: 0.8,
+  },
+})
+}>{label}</button> -->
